3754bcecf0e9c150c127e7020f9d4b628703d909
GoncaloCatarro <goncalocatarro)(@gmail.com>
2024-03-13 15:15:17 +0000

    startoffrontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
