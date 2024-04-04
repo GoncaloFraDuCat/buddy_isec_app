ffd122c72c9892b4573195555822e7ef1ac0006d
GoncaloCatarro <goncalocatarro)(@gmail.com>
2024-04-04 15:39:31 +0100

    fixed migrations bug

--- db/migrate/20240318121419_create_mentorship_requests.rb
+++ db/migrate/20240318121419_create_mentorship_requests.rb
@@ -1,7 +1,6 @@
 class CreateMentorshipRequests < ActiveRecord::Migration[7.1]
   def change
     create_table :mentorship_requests do |t|
-      t.references :mentor, null: false, foreign_key: true
       t.string :status
 
       t.timestamps
--- db/migrate/20240318125337_remove_foreign_key_from_mentorship_requests.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForeignKeyFromMentorshipRequests < ActiveRecord::Migration[7.1]
-  def change
-    remove_foreign_key :mentorship_requests, :mentors
-  end
-end
