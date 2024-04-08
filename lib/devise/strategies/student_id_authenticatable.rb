4ab64d5e65248cd77b17d9f13dc206a453e26bea
GoncaloCatarro <goncalocatarro)(@gmail.com>
2024-04-08 12:19:00 +0100

    dbseeds and log in css done

--- lib/devise/strategies/student_id_authenticatable.rb
+++ lib/devise/strategies/student_id_authenticatable.rb
@@ -1,22 +1,22 @@
 require 'devise/strategies/authenticatable'
 
 module Devise
-  module Strategies
+ module Strategies
     class StudentIdAuthenticatable < Authenticatable
       def valid?
-        params[:user] && params[:user][:student_id].present?
+        params[:user] && params[:user][:student_id].present? && params[:user][:email].present?
       end
 
       def authenticate!
-        user = User.find_by(student_id: params[:user][:student_id])
+        user = User.find_by(student_id: params[:user][:student_id], email: params[:user][:email])
         if user
           success!(user)
         else
-          fail!('Invalid student ID')
+          fail!('Invalid student ID or email')
         end
       end
     end
-  end
+ end
 end
 
 Warden::Strategies.add(:student_id_authenticatable, Devise::Strategies::StudentIdAuthenticatable)
