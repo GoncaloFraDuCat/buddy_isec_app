a7eeeb259f890c56b871203d4ec98345e770d8fe
kauredo <vaskafig@gmail.com>
2024-04-09 20:31:48 +0100

    WIP

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -29,26 +29,22 @@ class Users::SessionsController < Devise::SessionsController
     @user = User.find(params[:id])
     redirect_to users_path, alert: 'User is not a mentor.' unless @user.mentor?
     protected
-
-    def after_sign_in_path_for(_resource)
-      # Specify the path you want to redirect users to after sign-in
-      # For example, to redirect to the profile page:
-      search_path
-      # Or, to redirect to the matches page:
-      # matches_path
-      # Or, to redirect to the search page:
-      # search_path
-    end
-    # DELETE /resource/sign_out
-    # def destroy
-    #   super
-    # end
-
-    # protected
-
-    # If you have extra params to permit, append them to the sanitizer.
-    # def configure_sign_in_params
-    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-    # end
   end
+
+  def after_sign_in_path_for(_resource)
+    # Specify the path you want to redirect users to after sign-in
+    # For example, to redirect to the profile page:
+    search_path
+    # Or, to redirect to the matches page:
+    # matches_path
+    # Or, to redirect to the search page:
+    # search_path
+  end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
