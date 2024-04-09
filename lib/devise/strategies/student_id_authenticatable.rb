a12f4e7b51b56f1dcdbda7c0314f322fd3b7ff7e
kauredo <vaskafig@gmail.com>
2024-04-09 22:56:22 +0100

    use only oci8

--- lib/devise/strategies/student_id_authenticatable.rb
+++ lib/devise/strategies/student_id_authenticatable.rb
@@ -1,4 +1,5 @@
 require 'devise/strategies/authenticatable'
+require 'oci8'
 
 module Devise
   module Strategies
@@ -10,12 +11,17 @@ module Devise
       end
 
       def authenticate!
+        user = nil
+
         # Establish a connection to the Oracle database
         binding.pry
-        oracle_db = ActiveRecord::Base.establish_connection(:oracle_db).connection
-
+        # oracle_db = ActiveRecord::Base.establish_connection(:oracle_db).connection
+        conn = OCI8.new('your_username', 'your_password', '//your_database_host:port/service_name')
         # Query the Oracle database for the user's credentials
-        user = oracle_db.exec_query("SELECT * FROM NOME_DA_TABLE_NO_ORACLE WHERE email = '#{params[:email]}' AND password = '#{params[:password]}'")
+        conn.exec("SELECT * FROM NOME_DA_TABLE_NO_ORACLE WHERE email = '#{params[:email]}' AND password = '#{params[:password]}'") do |row|
+          user = row
+        end
+        # user = oracle_db.exec_query("SELECT * FROM NOME_DA_TABLE_NO_ORACLE WHERE email = '#{params[:email]}' AND password = '#{params[:password]}'")
 
         if user # If the user is found in the Oracle database
           # If the user is found, fetch additional data and save it to the local PostgreSQL database
@@ -24,16 +30,25 @@ module Devise
           local_user = User.find_by(email: attrs['email'])
 
           if local_user.nil? # This is user's first time logging in to the Rails app
+            user_data = nil
             local_user = User.create(email: params[:email])
 
             # query oracle for additional data
-            user_data = oracle_db.exec_query('').first
+            # user_data = oracle_db.exec_query('').first
+            conn.exec('') do |row|
+              user_data = row
+            end
+
             local_user.update(user_data)
+
+            conn.logoff # disconnect from the Oracle database
           end
 
           # Sign in the user
           success!(local_user)
         else
+          conn.logoff # disconnect from the Oracle database
+
           # If the user is not found, fail the authentication
           fail!('Invalid email or password')
         end
