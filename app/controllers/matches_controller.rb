8ce41f55a39e8e34cc2fb6a91a655b2d730c741b
Goncalo Catarro <goncalofraducat@gmail.com>
2024-10-14 11:18:59 +0100

    little css

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,9 +1,9 @@
 class MatchesController < ApplicationController
   def index
-    @mentee_requests = if current_user.mentor?
-                         MentorshipRequest.where(mentor_id: current_user.id)
-                       else
-                         MentorshipRequest.where(mentee_id: current_user.id).where(status: %w[pending accepted])
-                       end
+    @mentee_requests = if @user.mentor?
+                        MentorshipRequest.where(mentor_id: @user.id)
+                      else
+                        MentorshipRequest.where(mentee_id: @user.id).where(status: %w[pending accepted])
+                      end
   end
 end
